9b5917c08aee7c43ab5cd6bc1c808e61cba05ba9
Tony Carbetta <tony@Tonys-MacBook-Pro.local>
2018-04-08 19:39:41 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+var firstName = prompt ("What is your first name?");
+var lastName = prompt ("What is your last name?");
+var age = prompt ("What old are you? (Years)");
+
+var months = age * 12;
+var days =  age * 365;
+
+
+var message = alert ("Hello " + firstName  + " " + lastName + " "  + "you are " + months + " months and " + days + " days old!");
